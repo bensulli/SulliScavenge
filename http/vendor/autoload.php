1326e6ed63938b54004e3bdd1f23de28112ed0dc
Kyle Harrison <silent.coyote1@gmail.com>
2014-06-30 23:59:30 -0700

    yay repositories now work again

--- http/vendor/autoload.php
+++ http/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit7f9a4e86a9256c5481a4951cd0550794::getLoader();
+return ComposerAutoloaderInit9d6e168311da2cbef8860f0a83a023db::getLoader();
